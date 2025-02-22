270d9c307fca1a737f678c54dc03d66c43c8a861
Dhruv <dhruvsha0507@gmail.com>
2025-02-23 10:13:42 +1100

    Add image + Change to Projects section

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-        entry.target.classList.add('showing');
+            if (entry.target.classList.contains('hidden')){
+                entry.target.classList.add('showing');
+            }
+            else if (entry.target.classList.contains('hide')){
+                entry.target.classList.add('show');
+            }
         } else {
-        entry.target.classList.remove('showing');
+            if (entry.target.classList.contains('showing')){
+                entry.target.classList.remove('showing');
+            }
+            else if (entry.target.classList.contains('show')){
+                entry.target.classList.remove('show');
+            }
         }
     });
 });
 
 const hidden = document.querySelectorAll('.hidden');
 hidden.forEach((element) => {
+    observer.observe(element);
+});
+
+const other_hidden = document.querySelectorAll('.hide');
+other_hidden.forEach((element) => {
     observer.observe(element);
 });
